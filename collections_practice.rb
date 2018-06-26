bdf99ef06a0a3421d9624a05e2b75974ec5dfecb
Justine Bricknell <justinegayton@gmail.com>
2018-06-26 19:40:30 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,11 +1,35 @@
+#ascending order
 def sort_array_asc (array)
   array.sort do |a, b|
     a <=> b
   end
 end
 
+#descending order
 def sort_array_desc(array)
   array.sort do |a, b|
     a <=> begin
   end
+end
+
+def sort_array_char_count(array)
+end
+
+def swap_elements(array)
+end
+
+def reverse_array(array)
+end
+
+def kesha_maker(keshababy)
+#$$$$$$$$
+end
+
+def find_a(array)
+end
+
+def sum_array
+end
+
+def add_s(array)
 end
